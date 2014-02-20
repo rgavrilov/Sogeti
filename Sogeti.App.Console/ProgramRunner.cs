703b5e952baafa5564a1c268db762b1a94d447f2
rgavrilov <roman.gavrilov@gmail.com>
2014-02-20 07:47:15 -0700

    initial checkin

---
+++ Sogeti.App.Console/ProgramRunner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+
+namespace Sogeti.App.Console {
+    internal class ProgramRunner {
+        private static void Main(string[] args) {
+            var application = new Sogeti.App.Application();
+            application.Run(args);
+        }
+    }
+}
---
+++ Sogeti.App/Application.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using NConsoler;
+using Ninject;
+
+namespace Sogeti.App {
+    public class Application {
+        public void Run(string[] args) {
+            var kernel = new StandardKernel();
+            kernel.Bind<ICsvFileReader>().To<CsvFileReader>();
+            Consolery.Run(kernel.Get<Controller>(), args);
+        }
+    }
+
+    public class Controller {
+        private readonly ICsvFileReader fileReader;
+
+        public Controller(ICsvFileReader fileReader) {
+            this.fileReader = fileReader;
+        }
+
+        [Action(Description = "load and process CSV file with presidents")]
+        public void Load(
+            [NConsoler.Optional("sample.csv", Description = "file to read presidents from")] string inputFilepath) {
+            fileReader.Open();
+            string[] line;
+            while ((line = fileReader.GetNextLine()) != null) {
+                Console.WriteLine(line.First());
+            }
+        }
+    }
+
+    public interface ICsvFileReader {
+        void Open();
+        string[] GetNextLine();
+        void Close();
+    }
+
+
+    public class CsvFileReader : ICsvFileReader {
+        private int remainingLineCount;
+
+        public void Open() {
+            remainingLineCount = 5;
+        }
+
+        public string[] GetNextLine() {
+            if (remainingLineCount-- > 0) {
+                return new[] {"A", "B", "C"};
+            }
+            return null;
+        }
+
+        public void Close() {}
+    }
+}
